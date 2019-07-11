c6bd68900cedf363587eaefe4f611ae2b310ca3e
Nikhil Mishra <nikhil.mishra@knoldus.in>
2019-07-11 11:27:38 +0530

    Code updated.

--- src/matches.rs
+++ src/matches.rs
@@ -1,16 +1,17 @@
 pub fn run() {
+    println!("Enter a currency: ");
     let currency : String = read!();
     println!("{}", country(currency));
 }
 
 fn country (currency : String)-> String {
-    match currency {
-        AUD => String::from("AUSTRALIA"),
-        CAD => String::from("CANADA"),
-        INR => String::from("INDIA"),
-        PEN => String::from("PERU"),
-        RUB=> String::from("RUSSIA"),
-        USB => String::from("USA"),
+    match currency.as_ref() {
+        "AUD" => String::from("AUSTRALIA"),
+        "CAD" => String::from("CANADA"),
+        "INR" => String::from("INDIA"),
+        "PEN" => String::from("PERU"),
+        "RUB" => String::from("RUSSIA"),
+        "USD" => String::from("USA"),
         _ => String::from("NONE"),
     }
 }
